2ccc847fee6951cf124cf85f7afbd775b1f0035a
Thea House <theahouse10@gmail.com>
2019-08-11 19:22:30 +1000

    Installed lsr, psych, and car packages

---
+++ Scripts/First script.R
@@ -0,0 +1,14 @@
+seeker <- 3.1415 # create the first variable
+lover <- 2.7183 # create the second variable
+keeper <- seeker * lover # now multiply them to create a third one
+print(keeper)
+objects()
+ls()
+ls.str()
+who()
+library(lsr)
+install.packages(c("psych"))
+install.packages(c("lsr"))
+install.packages(c("car"))
+library(lsr)
+who()
